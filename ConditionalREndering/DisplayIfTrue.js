ee997f39dd9904f03590cacd1b26627373ea2055
jordan Moldovan <jmoldyvan@gmail.com>
2022-11-12 18:56:29 -0800

    display if true

---
+++ ConditionalREndering/DisplayIfTrue.js
@@ -0,0 +1,167 @@
+import React from "react"
+
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    /**
+     * Challenge:
+     * - Only display the punchline paragraph if `isShown` is true
+     */
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            <p>{props.punchline}</p>
+            <button onClick={toggleShown}>Show Punchline</button>
+            <hr />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react"
+
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    /**
+     * Challenge:
+     * - Only display the punchline paragraph if `isShown` is true
+     */
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleShown}>Show Punchline</button>
+            <hr />
+        </div>
+    )
+}
