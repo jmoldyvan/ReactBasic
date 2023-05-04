d9638253332480466d689b4ecf99734197121afa
jordan Moldovan <jmoldyvan@gmail.com>
2023-05-03 21:52:52 -0700

    axios

---
+++ lightAndDark/App.js
@@ -0,0 +1,139 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
+    return (
+        <div className="container">
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+}
